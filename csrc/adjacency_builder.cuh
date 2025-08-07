e79839beddd8ed5b1416b6435230159b6bf41593
alzoqm <70330480+alzoqm@users.noreply.github.com>
2025-08-07 12:45:18 +0900

    Update adjacency_builder.cuh

--- csrc/adjacency_builder.cuh
+++ csrc/adjacency_builder.cuh
@@ -1,4 +1,4 @@
-// csrc/adjacency_builder.cuh (수정됨)
+// csrc/adjacency_builder.cuh (수정된 파일)
 #pragma once
 
 #include <torch/extension.h>
@@ -10,9 +10,17 @@ std::pair<long, torch::Tensor> count_and_create_offsets_cuda(
     const torch::Tensor& population_tensor
 );
 
-// 2단계: 사전에 할당된 child_indices 텐서의 내용을 채우는 함수
+// 2단계: 사전에 할당된 child_indices 텐서의 내용을 채우고 정렬하는 함수
 void fill_child_indices_cuda(
     const torch::Tensor& population_tensor,
     const torch::Tensor& offset_array,
     torch::Tensor& child_indices // 입력이자 출력 (in-place modification)
+);
+
+// [신규] 자식 리스트를 정렬하는 커널을 호출하는 런처 함수 선언
+void launch_sort_children_kernel(
+    const int* offset_ptr,
+    int* child_indices_ptr,
+    int pop_size,
+    int max_nodes
 );
